c9510adc6dd2acbe543528bd2d8206fc438441d2
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2022-11-29 16:38:33 +0530

    Added CORS

--- index.js
+++ index.js
@@ -8,12 +8,30 @@ const authRoute = require("./routes/auth");
 dotenv.config();
 
 //DB Connection
-mongoose.connect(process.env.DB_PASSWORD, () => {
-  console.log("DB connected");
-});
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DB_PASSWORD);
+
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDatabase();
 
 //Middleware
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //Route Middleware
 app.use("/user", authRoute);
