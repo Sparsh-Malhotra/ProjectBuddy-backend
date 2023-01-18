3018a518b27acb9c8ca2f92177b62e1aee071bce
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2023-01-18 23:09:20 +0530

    Added hosted domain in allowed domains

--- index.js
+++ index.js
@@ -29,7 +29,14 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: true, exposedHeaders: "*" }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://project-buddy.live",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
